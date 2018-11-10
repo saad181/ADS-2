dedeb282cd87be1f3e7cfaeed636f4bdc5b7ff8b
saad181 <saadahmed181@msitprogram.net>
2018-11-10 12:11:41 +0530

    "ads2_m12_a1_1: 2/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ assignments/m12 exam/ADS-2_Exam-2/Edge.java
@@ -0,0 +1,223 @@
+/**
+ *class for edge.
+ */
+class Edge implements Comparable<Edge> {
+    /**
+     *the variable to store.
+     *vetexOne
+     */
+    private int vertexOne;
+    /**
+     *the variable to store.
+     *other vertex.
+     */
+    private int vertexTwo;
+    /**
+     *the variable to store the weight of.
+     *each edge.
+     */
+    private double weight;
+    /**
+     *the constructor to initialize the.
+     *vertices and their edge weight
+     * @param      v  vertexOne
+     * @param      w  vertexTwo
+     * @param      cost  weight of edge
+     */
+    Edge(final int v, final int w,
+         final double cost) {
+        this.vertexOne = v;
+        this.vertexTwo = w;
+        this.weight = cost;
+    }
+    /**
+     *this method returns the weight of edge.
+     *
+     * @return  weight of edge
+     */
+    public double weight() {
+        return this.weight;
+    }
+    /**
+     *this method returns one vertex.
+     *
+     * @return  one end of edge.
+     */
+    public int either() {
+        return vertexOne;
+    }
+    /**
+     *returns the other end of vertex.
+     *
+     * @param      v already connected vertex
+     *
+     * @return another vertex
+     */
+    public int other(final int v) {
+        if (v == vertexOne) {
+            return vertexTwo;
+        } else {
+            return vertexOne;
+        }
+    }
+    /**
+     *
+     *the method is to compare the two weights.
+     *of edges.
+     * @param      that  another edge
+     *
+     * @return  if this edge is  greater than that.
+     * returns 1.
+     * equal returns zero.
+     */
+    public int compareTo(final Edge that) {
+        if (this.weight < that.weight) {
+            return -1;
+        } else if (this.weight > that.weight) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    public String toString() {
+        return String.format("%d-%d %.5f",
+         vertexOne, vertexTwo, weight);
+    }
+}
+/**
+ * Class for edge weighted graph.
+ */
+class EdgeWeightedGraph {
+    /**
+     *the variable to store vertices.
+     */
+    private int vertices;
+    /**
+     *the variable to store edges.
+     */
+    private int edges;
+    /**
+     *the array of bags to store.
+     *complete adjacency list.
+     */
+    private Bag<Edge>[] adj;
+    /**
+     *the constructor to initialize.
+     *
+     * @param      v vertices count
+     */
+    EdgeWeightedGraph(final int v, final int e) {
+        this.vertices = v;
+        this.edges = e;
+        adj = (Bag<Edge>[]) new Bag[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adj[i] = new Bag<Edge>();
+        }
+    }
+    /**
+     *this method returns the vertices count.
+     *
+     * @return  number of vertices.
+     */
+    public int vertices() {
+        return this.vertices;
+    }
+    /**
+     *this method is to build the graph.
+     *
+     * @param      edge  The edge
+     */
+    public void addEdge(final Edge edge) {
+        int vertexOne = edge.either();
+        int vertexTwo = edge.other(vertexOne);
+        adj[vertexOne].add(edge);
+        adj[vertexTwo].add(edge);
+    }
+    /**
+     *returns the iterator to return the bag at that.
+     *vertex.
+     * @param      vertex  The vertex
+     *time complexity is O(V)
+     * @return iterator at that vertex.
+     */
+    public Iterable<Edge> adj(final int vertex) {
+        return adj[vertex];
+    }
+    /**
+     *returns all the edges in graph.
+     *time complexity is O(E)
+     * @return bag of all the edges.
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(vertices + " vertices " + edges + " edges" + "\n");
+        for (int v = 0; v < vertices; v++) {
+            s.append(v + ": ");
+            for (Edge e : adj[v]) {
+                s.append(e + "  ");
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+ }
+/**
+ *class for minimum spanning tree.
+ */
+// class MinST {
+//     /**
+//      *the graph object.
+//      */
+//     private EdgeWeightedGraph graph;
+//     /**
+//      *queue to give the mst.
+//      */
+//     private Queue<Edge> mst;
+//     /**
+//      *intializes  the values.
+//      *time complexity is O(ElogE).
+//      *where e is the edges.
+//      * @param      g  graph object.
+//      */
+//     MinST(final EdgeWeightedGraph g) {
+//         graph = g;
+//         mst = new Queue<Edge>();
+//         MinPQ<Edge> pq = new MinPQ<Edge>();
+//         for (Edge edge : graph.edges()) {
+//             pq.insert(edge);
+//         }
+//         Unionfind ufObj = new Unionfind(graph.vertices());
+//         while (!pq.isEmpty()
+//                 && mst.size() < graph.vertices() - 1) {
+//             Edge edge = pq.delMin();
+//             int vertexOne = edge.either();
+//             int vertexTwo = edge.other(vertexOne);
+//             if (!ufObj.connected(vertexOne, vertexTwo)) {
+//                 ufObj.union(vertexOne, vertexTwo);
+//                 mst.enqueue(edge);
+//             }
+//         }
+//     }
+//     /**
+//      *it returns all the edges on mst.
+//      *
+//      * @return  queue which contains all
+//      * vertices.
+//      */
+//     public Iterable<Edge> edges() {
+//         return mst;
+//     }
+//     /**
+//      *this method returns the total weight.
+//      *of mst.
+//      *time complexity is O(E)
+//      * @return weight of mst.
+//      */
+//     public double total() {
+//         double sum = 0.0;
+//         for (Edge e : edges()) {
+//             sum += e.weight();
+//         }
+//         return sum;
+//     }
+// }
---
+++ assignments/m12 exam/ADS-2_Exam-2/Solution.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+final class Solution {
+    private Solution() {
+
+    }
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int ver = Integer.parseInt(scan.nextLine());
+        int ed = Integer.parseInt(scan.nextLine());
+        Edge edge;
+        EdgeWeightedGraph ewg = new EdgeWeightedGraph(ver, ed);
+        for (int i = 0; i < ed; i++) {
+            String[] tokens = scan.nextLine().split(" ");
+            edge = new Edge(Integer.parseInt(tokens[0]),
+                            Integer.parseInt(tokens[1]),
+                            Double.parseDouble(tokens[2]));
+            ewg.addEdge(edge);
+        }
+        String caseToGo = scan.nextLine();
+        switch (caseToGo) {
+        case "Graph":
+            System.out.println(ewg);
+            break;
+
+        case "DirectedPaths":
+            // String[] input = scan.nextLine().split(" ");
+            // DijkstrasSP dij = new DijkstrasSP(ewg, Integer.parseInt(input[0]));
+            // if(dij.hasPathTo(Integer.parseInt(input[1]))) {
+            //     System.out.println(dij.distance(Integer.parseInt(input[1])));
+            // } else {
+            //     System.out.println("No Path Found.");
+            // }
+            break;
+        case "ViaPaths":
+            // String[] viapath = scan.nextLine().split(" ");
+            // DijkstrasSP dij1 = new DijkstrasSP(ewg, Integer.parseInt(viapath[0]));
+            // if(dij1.hasPathTo(Integer.parseInt(viapath[2])) && dij1.hasPathTo(Integer.parseInt(viapath[1]))) {
+            //     System.out.println(dij1.distance(Integer.parseInt(viapath[2])));
+            // } else {
+            //     System.out.println("No Path Found.");
+            // }
+            break;
+
+        default:
+            break;
+        }
+
+    }
+}
