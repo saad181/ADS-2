06e24d72002e07b1dd9e51aa1b8f87bb909118fc
saad181 <saadahmed181@msitprogram.net>
2018-11-25 18:18:05 +0530

    Solution.java

---
+++ assignments/m24 exam/Assignment 1/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.HashMap;
+class Solution{
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int num = Integer.parseInt(scan.nextLine());
+		BST<Integer, Integer> bst = new BST<Integer, Integer>();
+		for(int i =0;i<num;i++){
+			int temp = Integer.parseInt(scan.nextLine());
+			if(!bst.contains(temp)){
+				bst.put(temp, 1);
+			} else {
+				bst.put(temp, bst.get(temp)+1);
+			}
+		}
+		bst.display();
+		}
+}
